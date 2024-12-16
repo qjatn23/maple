e3b40f3df2b148063749b3d03511f3ce78ea4b3b
qjatn23 <beomsu97@naver.com>
2024-12-16 14:57:05 +0900

    36 commit2

--- articleapp/models.py
+++ articleapp/models.py
@@ -8,4 +8,4 @@ class Article(models.Model):
     image = models.ImageField(upload_to='article/', null=False)
     content = models.TextField(null=True)
 
-    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    created_at = models.DateField(auto_now_add=True, null=True)
