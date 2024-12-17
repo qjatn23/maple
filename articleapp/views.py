fba0ba7f5ea103d4fbde275259cf143b447915c5
qjatn23 <beomsu97@naver.com>
2024-12-17 11:43:22 +0900

    40 commit

--- articleapp/views.py
+++ articleapp/views.py
@@ -1,5 +1,4 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
 from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView, ListView
